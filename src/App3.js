797c989045ac8361031b8575bed60aa048a84a5a
Nathan McClorey <njmcclorey@gmail.com>
2024-10-28 11:21:09 -0400

    initial commit

---
+++ src/App3.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div id="App">
+        <Header></Header>
+        <p> Hi, my  name is Nathan</p>
+    </div>
+  );
+}
+
+
+function Header() {
+    return (
+      <div id="Header">
+          <p> This is a header</p>
+      </div>
+    );
+  }
+
+
+
+export default App;
